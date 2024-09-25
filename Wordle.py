cec838f72f44bafd6179c26ee9e24c57ebdba95c
kjbrown24 <kjbrown@willamette.edu>
2024-09-24 20:35:02 -0700

    Add files via upload

--- Wordle.py
+++ Wordle.py
@@ -1,29 +1,33 @@
 ########################################
-# Name:
-# Collaborators (if any):
+# Name:Kendall
+# Collaborators (if any):Tutors, Brooke/Haley
 # GenAI Transcript (if any):
 # Estimated time spent (hr):
 # Description of any added extensions:
 ########################################
 
-from WordleGraphics import *  # WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
-from english import * # ENGLISH_WORDS, is_english_word
+from WordleGraphics import WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
+from english import ENGLISH_WORDS, is_english_word
 import random
 
 def wordle():
-    # The main function to play the Wordle game.
-
-    def enter_action():
-        # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
-
-
-    gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-
+	gw = WordleGWindow()
+	
+	def display_word():
+		secret_word="pizza"
+		for i in range(N_COLS):
+			f = secret_word[i]
+			gw.set_square_letter(0,i,f)
+	display_word()
+		
+	def enter_action():
+		gw.show_message("You need to implement this method")
+	
+	
+	gw.add_enter_listener(enter_action)
 
 
 
 # Startup boilerplate
 if __name__ == "__main__":
-    wordle()
+	wordle()
